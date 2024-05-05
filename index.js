c567ebc72c847ba743a37a5f35e436f36dd7ab70
gta-pong <nomadpong@gmail.com>
2024-05-05 20:03:29 +0800

    starting new branch w mongoose.connect

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient } = require('mongodb');
-require("dotenv").config();
+// const { MongoClient } = require("mongodb");
+const mongoose = require('mongoose');
+const User = require("./models/user");
 
-// Connection URL
 const url = process.env.MONGO_URI;
-const client = new MongoClient(url);
+mongoose.connect(url);
 
-// Database Name
-const dbName = 'test';
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/api/users", bodyParser.json());
 
-// Define function to connect to mongodb server
-async function main() {
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('exerciseusers');
-  // Can add more code here:
 
-  return 'done.';
-};
 
-// Execute server connection
-main()
-  .then(console.log)
-  .catch(console.error)
-  .finally(() => client.close());
+// // Connection URL
+// const url = process.env.MONGO_URI;
+// const client = new MongoClient(url);
 
+// // Database Name
+// const dbName = "test";
+// const collectionName = "exerciseusers";
+
+// // Define function to connect to mongodb server
+// async function main() {
+//   await client.connect();
+//   console.log("Connected successfully to server");
+//   const db = client.db(dbName);
+//   const collection = db.collection("collectionName");
+//   // Can add more code here:
+//   return "done.";
+// }
+
+// // Execute server connection
+// main()
+//   .then(console.log)
+//   .catch(console.error)
+//   .finally(() => client.close());
 
 app.use(cors());
 app.use(express.static("public"));
@@ -35,19 +45,41 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// Define Endpoints
+
 app
-  .route("/users")
+  .route("/api/users")
   .get(function (req, res) {
     console.log("/users get fired");
     return res.send({ obj: "/users get fired" });
   })
-  .post(function (req, res) {
+  .post(async function (req, res) {
     console.log("/users post fired");
+    console.log(req.body.username);
+
+    // try {
+    //   const userData = req.body.username;
+
+    //   const db = client.db(dbName);
+    //   const usersCollection = db.collection(collectionName);
+    //   const result = await usersCollection.insertOne({userData});
+    //   res
+    //     .status(201)
+    //     .json({
+    //       message: "User created successfully",
+    //       insertedUser: result.ops[0],
+    //     });
+    // } catch (error) {
+    //   console.error("Error inserting user:", error);
+    //   res.status(500).json({ message: "Internal server error" });
+    // }
+
     return res.send({ obj: "/users post fired" });
   });
 
-app.post("/users/:_id/exercises", function (req, res) {
+app.post("/api/users/:_id/exercises", function (req, res) {
   console.log("/users/:_id/exercises post fired");
+
   return res.send({ obj: "/users/:_id/exercises post fired" });
 });
 
---
+++ models/user.js
@@ -0,0 +1,7 @@
+let mongoose = require("mongoose");
+
+let userSchema = new mongoose.Schema({
+  username: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
